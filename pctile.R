f6fc761e9a7c8518f18707d296b61215d204ffd3
GuliTeshabay <nasirova.guli@gmail.com>
2024-03-25 20:29:48 +0100

    cleaning activities on the way

--- pctile.R
+++
@@ -1,9 +0,0 @@
-
-pctile<-function (x, pct) {
-  x<-matrix(sort(x), dim(x)[1], 1)
-  pct<-round(pct*dim(x)[1])
-  xpct<-x[pct]
-  return(xpct)
-}
-
-
--- smpl.R
+++
@@ -1,8 +0,0 @@
-
-smpl<- function (calvec, nfirst, nlast, nper){
-  i1<-nfirst[1]+(nfirst[2]-1)/nper
-  i2<-nlast[1]+(nlast[2]-1)/nper
-  tmp1<-(calvec> (i1-0.0001))*1
-  tmp2<-(calvec< (i2+0.0001))*1
-  ismpl <- tmp1 * tmp2
-}
